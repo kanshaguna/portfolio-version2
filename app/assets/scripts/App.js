292ab6ccc6205fa1c72c7e03568eb7e6341ddbe6
kanshaguna <kansh1111@yahoo.com>
2021-06-30 11:57:41 +0800

    Image optimization

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,13 +1,18 @@
 import "../styles/styles.css";
 import 'lazysizes';
 import MobileMenu from "./modules/MobileMenu"
+import RevealOnScroll from './modules/RevealOnScroll.js';
 import StickyHeader from './modules/StickyHeader.js'
 import Particles from './modules/Particles.js';
 
 
 let mobileMenu = new MobileMenu();
 new StickyHeader();
-new Particles()
+new Particles();
+new Particles();
+new RevealOnScroll(document.querySelectorAll(".skills--description"), 75)
+new RevealOnScroll(document.querySelectorAll(".flex-container"), 75)
+new RevealOnScroll(document.querySelectorAll(".mainBoxBlack--contactText"), 75)
 
 
 if (module.hot) {
