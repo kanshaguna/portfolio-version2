de47f3aca43336e8b57fbb8e2a531b27bef17f28
kanshaguna <kansh1111@yahoo.com>
2021-07-08 16:17:44 +0800

    Minor edits

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,13 +3,13 @@ import 'lazysizes';
 import MobileMenu from "./modules/MobileMenu"
 import RevealOnScroll from './modules/RevealOnScroll.js';
 import StickyHeader from './modules/StickyHeader.js'
-import Particles from './modules/Particles.js';
+
 
 
 let mobileMenu = new MobileMenu();
 new StickyHeader();
-new Particles();
-new Particles();
+
+
 new RevealOnScroll(document.querySelectorAll(".skills--description"), 75)
 new RevealOnScroll(document.querySelectorAll(".flex-container"), 75)
 new RevealOnScroll(document.querySelectorAll(".mainBoxBlack--contactText"), 75)
